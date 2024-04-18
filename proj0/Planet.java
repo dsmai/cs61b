b4cb0362b7dc935f0f7758bbaf243ddd67f493e3
Son Mai <son.dmai@gmail.com>
2024-04-18 14:25:39 -0400

    first commit

---
+++ proj0/Planet.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    // constructor
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    // another constructor, take in a Planet and initialize an identical Planet object (a copy)
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass  = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    public double calDistance(Planet p) {
+        return distance;
+    }
+
+}
