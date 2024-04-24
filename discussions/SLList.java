d5178decf1b5261711ec533c2b80aa4aed425417
dsmai <son.dmai@gmail.com>
2024-04-24 00:29:44 -0400

    Worked on SLList class.

--- discussions/SLList.java
+++ discussions/SLList.java
@@ -9,10 +9,13 @@ public class SLList {
     public SLList(int number) {
         first = new IntNode(number, null);
         last = first;
+        size = 1;
     }
 
     public void addFirst(int number) {
+        last = first;
         first = new IntNode(number, first);
+        size++;
     }
 
     public int getFirst() {
@@ -25,5 +28,53 @@ public class SLList {
 
     public void addLast(int number) {
         last.next = new IntNode(number, null);
+    }
+
+    public void addLastIterative(int number) {
+        if (first != null) {
+            IntNode ptr = first;
+
+            // Advance pointer ptr to the end of the list
+            while (ptr.next != null) {
+                ptr = ptr.next;
+            }
+            // when out of while loop, we are at the last node
+            ptr.next = new IntNode(number, null);
+        } else {
+            first = new IntNode(number, null);
+        }
+
+        size++;
+    }
+
+    public void addLastRecursive(int number) {
+        if (first.next == null) {
+            first.next = new IntNode(number, null);
+        }
+        first.next.addLastRecursive(number);
+
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int sizeIterative() {
+        int totalSize = 0;
+        IntNode ptr = first;
+        while (ptr != null) {
+            totalSize++;
+            ptr = ptr.next;
+        }
+        return totalSize;
+    }
+
+    public int sizeRecursive() {
+        // base case: .next is null, return 1
+        if (first.next == null) {
+            return 1;
+        }
+        return 1 + first.next.sizeRecursive();
     }
 }
