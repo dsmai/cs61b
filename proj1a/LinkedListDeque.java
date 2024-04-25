7836118d8a4b55a7c71218b202c8d1fa1e99850d
dsmai <son.dmai@gmail.com>
2024-04-25 16:05:50 -0400

    Modified LinkedListDeque ADT to not include first and last, only include sentinel node.

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -23,9 +23,7 @@ public class LinkedListDeque<T> {
 
     // Default constructor: empty deque
     public LinkedListDeque() {
-        Node sentinel = new Node(null, null, null); // What to pass in as first parameter here?
-        first = null;
-        last = null;
+        sentinel = new Node(null, null, null); // What to pass in as first parameter here?
         size = 0;
     }
 
@@ -38,6 +36,12 @@ public class LinkedListDeque<T> {
         size += 1;
     }
 
+    public LinkedListDequeAnother(T item) {
+        Node newNode = new Node(item, null, null);
+        sentinel = new Node(null, newNode, newNode);
+        size += 1;
+    }
+
     public void addFirst(T item) {
         Node newNode = new Node(item, sentinel, first);
         sentinel.next = newNode;
@@ -46,11 +50,25 @@ public class LinkedListDeque<T> {
         size += 1;
     }
 
+    public void addFirstAnother(T item) {
+        Node newNode = new Node(item, sentinel, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size += 1;
+    }
+
     public void addLast(T item) {
         Node newNode = new Node(item, last, null);
         last.next = newNode;
         sentinel.prev = newNode;
         last = newNode;
+        size += 1;
+    }
+
+    public void addLastAnother(T item) {
+        Node newNode = new Node(item, sentinel.prev, null);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
         size += 1;
     }
 
@@ -63,11 +81,27 @@ public class LinkedListDeque<T> {
         return resultNode.item; // need to think about null case?
     }
 
+    public T removeFirstAnother() {
+        Node resultNode = sentinel.next;
+        sentinel.next =  sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size -= 1;
+        return resultNode.item; // need to think about null case?
+    }
+
     public T removeLast() {
         Node resultNode = last;
         last.prev.next = null;
         sentinel.prev = last.prev;
         last = last.prev;
+        size -= 1;
+        return resultNode.item;
+    }
+
+    public T removeLastAnother() {
+        Node resultNode = sentinel.prev;
+        sentinel.prev.prev.next = null;
+        sentinel.prev = sentinel.prev.prev;
         size -= 1;
         return resultNode.item;
     }
@@ -84,11 +118,12 @@ public class LinkedListDeque<T> {
         if (index >= size) {
             return null;
         } else {
-            int location = 0;
-            // Create pointer reference to "first" of deque
-            Node ptr = first;
-            while (location < index) {
-                location += 1;
+            // Create pointer reference to "first" of deque, or index 0
+            Node ptr = sentinel.next;
+
+            //Use for loop to traverse the deque instead
+            for (int i = 0; i < size; i++) {
+                // travers the list by moving the pointer
                 ptr = ptr.next;
             }
             return ptr.item;
