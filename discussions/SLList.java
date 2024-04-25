9d8336d9994744f660c9032fc5e7edfdb404cdf0
dsmai <son.dmai@gmail.com>
2024-04-24 23:26:57 -0400

    Added nested class IntNode to SLList class.

--- discussions/SLList.java
+++ discussions/SLList.java
@@ -6,6 +6,19 @@ public class SLList {
     private IntNode sentinel; // a special node that is always there in the beginning (to make empty and regular linked list behave the same)
     private IntNode last;     // the address of the tail of the linked list (cached)
 
+    // Nested class
+    public static class IntNode {
+        public int item;
+        public IntNode next;
+        public IntNode prev; // This makes our list have 2 links for every node (Doubly linked list)
+    
+        // constructor
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
     // default constructor, empty list
     public SLList() {
         first = null;
