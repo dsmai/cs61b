a3f79a3ce42b13fb8e6c75811ee7d86ba14cfe95
dsmai <son.dmai@gmail.com>
2024-04-28 00:30:02 -0400

    Implemented ArrayDeque class.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,46 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
     /** Array-based list implementation */
+    private int size;
+    private T[] items;
 
+    // Default constructor
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        return;
+    }
+
+    public void addLast(T item) {
+        return;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i]);
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        return;
+    }
+
+    public T removeLast() {
+        return;
+    }
+
+    public T get(int index) {
+        return;
+    }
 }
