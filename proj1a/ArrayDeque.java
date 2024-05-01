432e6f934163ebe523a4e3e597dba4c43fed1bf7
dsmai <son.dmai@gmail.com>
2024-05-01 11:30:17 -0400

    Updated ArrayDeque class.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,12 +1,12 @@
 public class ArrayDeque<T> {
     /** Array-based list implementation */
-    public int size;
-    public T[] items;
-    public int nextFirst; // index of the array when next addFirst will go to
-    public int nextLast; // index of the array when next addLast will go to
-    public double usageRatio;
+    private int size;
+    private T[] items;
+    private int nextFirst; // index of the array when next addFirst will go to
+    private int nextLast; // index of the array when next addLast will go to
+    private double usageRatio;
 
-    // Default constructor, create empty array deque
+    /** Default constructor */
     public ArrayDeque() {
         items = (T[]) new Object[8]; // {0, 0, 0, 0, 0, 0, 0, 0}
         nextFirst = 3;
@@ -15,63 +15,80 @@ public class ArrayDeque<T> {
         usageRatio = 0.0;
     }
 
+    /**
+     * Private helper method to resize (up or down)
+     * @param newSize
+     */
     private void resize(int newSize) {
         T[] newArray = (T[]) new Object[newSize];
-//        System.arraycopy(this.items, 0, newArray, 0, size);
-        // Copy the old array to the new bigger array, start copying from index [nextFirst + 1] till [nextLast]
-        int current = 0;
-        for (int i = nextFirst + 1; current < size; i++) {
-            if (i == items.length) {
-                i = 0;
-            }
-            newArray[(newArray.length / 4) + current] = this.items[i];
-            current++;
+        // System.arraycopy(this.items, 0, newArray, 0, size);
+
+        // Copy the old array to the new bigger array,
+        // starting copying from index [nextFirst + 1]
+//        int current = 0; // a counter
+//        for (int i = nextFirst + 1; current < size; i++) {
+//            if (i == items.length) {
+//                i = 0;
+//            }
+//            // place the copied items in the "middle" of the new array
+//            newArray[(newArray.length / 4) + current] = this.items[i];
+//            current++;
+//        }
+
+        for (int i = 0; i < size; i++) {
+            newArray[(newArray.length / 4) + i] = this.get(i);
         }
+
+        // make this.items now hold the memory address of newArray
         this.items = newArray;
+
+        // Update usage ratio
         usageRatio = (double) size / items.length;
+
         // I forgot to do something with nextFirst and nextLast when resize?
+        // Update nextFirst and nextLast with respect to newArray
         nextFirst = (newArray.length / 4) - 1;
         nextLast = (newArray.length - 1) - nextFirst;
     }
 
+    /**
+     * Private helper method to update nextFirst
+     */
     private void updateNextFirst() {
-        if (nextFirst == items.length) {
-            nextFirst = 0;
-        }
-        if (nextFirst == -1) {
-            nextFirst = items.length - 1;
-        }
+        nextFirst = (nextFirst + items.length) % items.length;
     }
 
+    /**
+     * Private helper method to update nextLast
+     */
     private void updateNextLast() {
-        if (nextLast == items.length) {
-            nextLast = 0;
-        }
-        if (nextLast == -1) {
-            nextLast = items.length - 1;
-        }
+        nextLast = (nextLast + items.length) % items.length;
     }
 
     public void addFirst(T item) {
         // check if run out of allocated memory box
+        // if yes, resize up
         if (size == items.length) {
             resize(size * 2);
         }
 
-        // how do we treat edge case?
-        // when nextFirst is already at 0
-        // => need to loop forward to the end of array
         items[nextFirst] = item;
 
         nextFirst--; // push nextFirst forward by 1
+
+        // to check edge case (nextFirst = -1)
         updateNextFirst();
 
+        // increase size
         size++;
+
+        // update usage ratio
         usageRatio = (double) size / items.length;
     }
 
     public void addLast(T item) {
         // check if run out of allocated memory box
+        // if yes, resize up
         if (size == items.length) {
             resize(size * 2);
         }
@@ -79,9 +96,14 @@ public class ArrayDeque<T> {
         items[nextLast] = item;
 
         nextLast++; // push nextLast back by 1
+
+        // to check edge case (nextLast = items.length)
         updateNextLast();
 
+        // increase size
         size++;
+
+        // update usage ratio
         usageRatio = (double) size / items.length;
     }
 
@@ -93,16 +115,23 @@ public class ArrayDeque<T> {
         return size;
     }
 
-    public void printDeque() {
-        // Have to print from nextFirst + 1 to nextLast - 1
-        int current = 0;
+//    public void printDeque() {
+//        // Have to print from nextFirst + 1 to nextLast - 1
+//        int current = 0;
+//
+//        for (int i = nextFirst + 1; current < size; i++) {
+//            if ( i == items.length) {
+//                i = 0;
+//            }
+//            System.out.print(items[i] + " ");
+//            current++;
+//        }
+//        System.out.println();
+//    }
 
-        for (int i = nextFirst + 1; current < size; i++) {
-            if ( i == items.length) {
-                i = 0;
-            }
-            System.out.print(items[i] + " ");
-            current++;
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[(getFirstIndex() + i) % items.length] + " ");
         }
         System.out.println();
     }
@@ -121,6 +150,7 @@ public class ArrayDeque<T> {
         // update the nextFirst pointer
         nextFirst = getFirstIndex();
 
+        // decrease size
         size--;
 
         // update usage ratio
@@ -146,6 +176,7 @@ public class ArrayDeque<T> {
         // update the nextLast pointer
         nextLast = getLastIndex();
 
+        // decrease size
         size--;
 
         // update usage ratio
@@ -158,51 +189,45 @@ public class ArrayDeque<T> {
         return last;
     }
 
+    /**
+     * Get the item at the given index
+     * Where 0 is the front, 1 is the next item
+     * If no such item exists, or index >= size, return null
+     * @param index
+     * @return
+     */
     public T get(int index) {
-        return items[index];
+        if (index >= size) {
+            return null;
+        }
+        return items[(getFirstIndex() + index) % items.length];
     }
 
     private T getFirst() {
-        // what if nextFirst is already at the end, or (size - 1) index
-        if (nextFirst == size - 1) {
-            return items[0];
-        } else {
-            return items[nextFirst + 1];
-        }
+        // what if nextFirst is already at the end, or (items.length - 1) index
+        return items[getFirstIndex()];
     }
 
     private int getFirstIndex() {
-        // what if nextFirst is already at the end, or (size - 1) index
-        if (nextFirst == size - 1) {
-            return 0;
-        } else {
-            return nextFirst + 1;
-        }
+        // what if nextFirst is already at the end, or (items.length - 1) index
+        return (nextFirst + 1) % items.length;
     }
 
     private T getLast() {
         // what if nextLast is already at the beginning, or 0 index
-        if (nextLast == 0) {
-            return items[size - 1];
-        } else {
-            return items[nextLast - 1];
-        }
+        return items[getLastIndex()];
     }
 
     private int getLastIndex() {
         // what if nextLast is already at the beginning, or 0 index
-        if (nextLast == 0) {
-            return size - 1;
-        } else {
-            return nextLast - 1;
-        }
+        return ((nextLast - 1) + items.length) % items.length;
     }
 
-    public int getLength() {
+    private int getLength() {
         return items.length;
     }
 
-    public double getUsageRatio() {
+    private double getUsageRatio() {
         return usageRatio;
     }
 }
