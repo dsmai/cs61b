13c5a82103f8ea7ecac511b88060b4b37b78953c
dsmai <son.dmai@gmail.com>
2024-05-01 14:11:04 -0400

    Refactored ArrayDeque.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -15,6 +15,14 @@ public class ArrayDeque<T> {
         usageRatio = 0.0;
     }
 
+    private int getLength() {
+        return items.length;
+    }
+
+    private double getUsageRatio() {
+        return usageRatio;
+    }
+
     /**
      * Private helper method to resize (up or down)
      * @param newSize
@@ -25,16 +33,6 @@ public class ArrayDeque<T> {
 
         // Copy the old array to the new bigger array,
         // starting copying from index [nextFirst + 1]
-//        int current = 0; // a counter
-//        for (int i = nextFirst + 1; current < size; i++) {
-//            if (i == items.length) {
-//                i = 0;
-//            }
-//            // place the copied items in the "middle" of the new array
-//            newArray[(newArray.length / 4) + current] = this.items[i];
-//            current++;
-//        }
-
         for (int i = 0; i < size; i++) {
             newArray[(newArray.length / 4) + i] = this.get(i);
         }
@@ -51,18 +49,42 @@ public class ArrayDeque<T> {
         nextLast = nextFirst + size + 1;
     }
 
-    /**
-     * Private helper method to update nextFirst
-     */
+    /** Private helper method to update nextFirst */
     private void updateNextFirst() {
         nextFirst = (nextFirst + items.length) % items.length;
     }
 
-    /**
-     * Private helper method to update nextLast
-     */
+    /** Private helper method to update nextLast */
     private void updateNextLast() {
         nextLast = (nextLast + items.length) % items.length;
+    }
+
+    private int getFirstIndex() {
+        // what if nextFirst is already at the end, or (items.length - 1) index
+        return (nextFirst + 1) % items.length;
+    }
+
+    private int getLastIndex() {
+        // what if nextLast is already at the beginning, or 0 index
+        return ((nextLast - 1) + items.length) % items.length;
+    }
+
+    private T getFirst() {
+        // what if nextFirst is already at the end, or (items.length - 1) index
+        return items[getFirstIndex()];
+    }
+
+    private T getLast() {
+        // what if nextLast is already at the beginning, or 0 index
+        return items[getLastIndex()];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
     }
 
     public void addFirst(T item) {
@@ -107,37 +129,28 @@ public class ArrayDeque<T> {
         usageRatio = (double) size / items.length;
     }
 
-    public boolean isEmpty() {
-        return size == 0;
+    /**
+     * Get the item at the given index
+     * Where 0 is the front, 1 is the next item
+     * If no such item exists, or index >= size, return null
+     * @param index
+     * @return
+     */
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        return items[(getFirstIndex() + index) % items.length];
     }
-
-    public int size() {
-        return size;
-    }
-
-//    public void printDeque() {
-//        // Have to print from nextFirst + 1 to nextLast - 1
-//        int current = 0;
-//
-//        for (int i = nextFirst + 1; current < size; i++) {
-//            if ( i == items.length) {
-//                i = 0;
-//            }
-//            System.out.print(items[i] + " ");
-//            current++;
-//        }
-//        System.out.println();
-//    }
 
     public void printDeque() {
         for (int i = 0; i < size; i++) {
-            System.out.print(items[(getFirstIndex() + i) % items.length] + " ");
+            System.out.print(get(i) + " ");
         }
         System.out.println();
     }
 
     public T removeFirst() {
-        // edge case, array deque is already empty, return null
         if (isEmpty()) {
             return null;
         }
@@ -157,6 +170,7 @@ public class ArrayDeque<T> {
         usageRatio = (double) size / items.length;
 
         // halve the array length if usage ratio is below 0.25
+        // for array of length 16 or more
         if (usageRatio < 0.25 && items.length >= 16) {
             resize(items.length / 2);
         }
@@ -188,47 +202,5 @@ public class ArrayDeque<T> {
             resize(items.length / 2);
         }
         return last;
-    }
-
-    /**
-     * Get the item at the given index
-     * Where 0 is the front, 1 is the next item
-     * If no such item exists, or index >= size, return null
-     * @param index
-     * @return
-     */
-    public T get(int index) {
-        if (index >= size) {
-            return null;
-        }
-        return items[(getFirstIndex() + index) % items.length];
-    }
-
-    private T getFirst() {
-        // what if nextFirst is already at the end, or (items.length - 1) index
-        return items[getFirstIndex()];
-    }
-
-    private int getFirstIndex() {
-        // what if nextFirst is already at the end, or (items.length - 1) index
-        return (nextFirst + 1) % items.length;
-    }
-
-    private T getLast() {
-        // what if nextLast is already at the beginning, or 0 index
-        return items[getLastIndex()];
-    }
-
-    private int getLastIndex() {
-        // what if nextLast is already at the beginning, or 0 index
-        return ((nextLast - 1) + items.length) % items.length;
-    }
-
-    private int getLength() {
-        return items.length;
-    }
-
-    private double getUsageRatio() {
-        return usageRatio;
     }
 }
